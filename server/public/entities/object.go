39d826281281460dc1fb49bec7a72028e8d691ee
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-06 10:39:47 +0530

    fix: updated object to have upload status

--- server/public/entities/object.go
+++ server/public/entities/object.go
@@ -8,5 +8,6 @@ type Object struct {
 	Name       string    `json:"name"`
 	MimeType   string    `json:"mime_type"`
 	ObjectSize int64     `json:"object_size"`
+	Status     string    `json:"status"`
 	CreatedAt  time.Time `json:"created_at"`
 }
