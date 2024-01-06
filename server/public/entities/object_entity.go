683f51229c6697acccc7342fa93bb758e246f152
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-06 19:09:06 +0530

    refactor: renamed Status to UploadStatus

--- server/public/entities/object_entity.go
+++ server/public/entities/object_entity.go
@@ -3,12 +3,12 @@ package entities
 import "time"
 
 type Object struct {
-	Id         string     `json:"id"`
-	BucketId   string     `json:"bucket_id"`
-	Name       string     `json:"name"`
-	MimeType   string     `json:"mime_type"`
-	ObjectSize int64      `json:"object_size"`
-	Status     string     `json:"status"`
-	CreatedAt  time.Time  `json:"created_at"`
-	UpdatedAt  *time.Time `json:"updated_at"`
+	Id           string     `json:"id"`
+	BucketId     string     `json:"bucket_id"`
+	Name         string     `json:"name"`
+	MimeType     string     `json:"mime_type"`
+	ObjectSize   int64      `json:"object_size"`
+	UploadStatus string     `json:"upload_status"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    *time.Time `json:"updated_at"`
 }
