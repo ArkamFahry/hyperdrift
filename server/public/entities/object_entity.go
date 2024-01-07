dd1fe6f2d9666b53f5047486101d67b7cf5d9f85
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-07 07:01:10 +0530

    chore: added metadata tag to object entity to store extra data

--- server/public/entities/object_entity.go
+++ server/public/entities/object_entity.go
@@ -3,12 +3,13 @@ package entities
 import "time"
 
 type Object struct {
-	Id           string     `json:"id"`
-	BucketId     string     `json:"bucket_id"`
-	Name         string     `json:"name"`
-	MimeType     string     `json:"mime_type"`
-	ObjectSize   int64      `json:"object_size"`
-	UploadStatus string     `json:"upload_status"`
-	CreatedAt    time.Time  `json:"created_at"`
-	UpdatedAt    *time.Time `json:"updated_at"`
+	Id           string         `json:"id"`
+	BucketId     string         `json:"bucket_id"`
+	Name         string         `json:"name"`
+	MimeType     string         `json:"mime_type"`
+	ObjectSize   int64          `json:"object_size"`
+	Metadata     map[string]any `json:"metadata"`
+	UploadStatus string         `json:"upload_status"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    *time.Time     `json:"updated_at"`
 }
