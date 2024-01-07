cbc8389f7388b1c6fc2cb9289a52430f186713d6
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-07 08:20:52 +0530

    chore: added string is empty or zero validator

--- server/packages/validators/strings.go
+++ server/packages/validators/strings.go
@@ -6,6 +6,10 @@ func IsEmptyString(value string) bool {
 	return strings.TrimSpace(value) == ""
 }
 
+func IsZeroOrEmptyString(value string) bool {
+	return strings.TrimSpace(value) == "" || strings.TrimSpace(value) == "0"
+}
+
 func ContainsAnyWhiteSpaces(value string) bool {
 	return strings.ContainsAny(value, " \t\r\n")
 }
