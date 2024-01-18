be27ecf8e968ca038609efe1f07ca89d023cdb45
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-18 21:11:51 +0530

    chore: added the base data models belonging to the system objects and buckets

---
+++ storage/server/models/bucket.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Bucket struct {
+	Id                   string     `json:"id"`
+	Name                 string     `json:"name"`
+	AllowedMimeTypes     []string   `json:"allowed_mime_types"`
+	MaxAllowedObjectSize int64      `json:"max_allowed_object_size"`
+	Public               bool       `json:"public"`
+	Disabled             bool       `json:"enabled"`
+	Locked               bool       `json:"locked"`
+	LockReason           string     `json:"lock_reason"`
+	LockedAt             *time.Time `json:"locked_at"`
+	CreatedAt            time.Time  `json:"created_at"`
+	UpdatedAt            *time.Time `json:"updated_at"`
+}
