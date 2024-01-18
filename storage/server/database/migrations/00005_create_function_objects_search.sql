e1992ad9157c2aa966a7901397923ef12a4720bf
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-18 21:07:01 +0530

    feat: added a storage objects search function to search object by object path

---
+++ storage/server/database/migrations/00005_create_function_objects_search.sql
@@ -0,0 +1,55 @@
+-- +goose Up
+-- +goose StatementBegin
+
+create or replace function storage.objects_search(bucket_name text, path_prefix text,
+                                                  levels int default 1, limits int default 100, offsets int default 0)
+    returns table
+            (
+                id               text,
+                name             text,
+                bucket           text,
+                mime_type        text,
+                size             bigint,
+                public           boolean,
+                metadata         jsonb,
+                upload_status    text,
+                last_accessed_at timestamptz,
+                created_at       timestamptz,
+                updated_at       timestamptz
+            )
+    language plpgsql
+as
+$$
+begin
+    return query
+        with files_folders as (select path_tokens[levels] as folder
+                               from storage.objects
+                               where objects.name ilike path_prefix || '%'
+                                 and objects.bucket_id = bucket_name
+                               group by folder
+                               limit limits offset offsets)
+        select objects.id               as id,
+               files_folders.folder     as name,
+               bucket_name              as bucket,
+               objects.mime_type        as mime_type,
+               objects.size             as size,
+               objects.public           as public,
+               objects.metadata         as metadata,
+               objects.upload_status    as upload_status,
+               objects.last_accessed_at as last_accessed_at,
+               objects.created_at       as created_at,
+               objects.updated_at       as updated_at
+        from files_folders
+                 left join storage.objects
+                           on path_prefix || files_folders.folder = objects.name and objects.bucket_id = bucket_name;
+end
+$$;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+drop function if exists storage.objects_search(text, text, int, int, int);
+
+-- +goose StatementEnd
