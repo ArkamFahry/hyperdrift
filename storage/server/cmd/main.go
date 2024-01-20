7e94dedef98c577c08d7202116605697edb89ab6
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-20 20:41:10 +0530

    chore: did the api server setup on the main start function

--- storage/server/cmd/main.go
+++ storage/server/cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/ArkamFahry/hyperdrift/storage/server/api"
+	"github.com/ArkamFahry/hyperdrift/storage/server/config"
+	"github.com/ArkamFahry/hyperdrift/storage/server/logger"
+	"github.com/spf13/viper"
+)
+
 func main() {
-	// Todo implement this
+	newViper := viper.New()
+	newLogger := logger.NewLogger(newViper)
+	newConfig := config.NewConfig(newViper, newLogger)
+
+	api.NewApi(newLogger, newConfig)
 }
