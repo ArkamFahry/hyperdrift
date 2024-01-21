b42278dffadd359b4a642ac9fc93f70e5a0ea89d
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-21 18:29:29 +0530

    refactor: moved config model into config package

--- storage/server/config/config.go
+++ storage/server/config/config.go
@@ -1,21 +1,30 @@
 package config
 
 import (
-	"github.com/ArkamFahry/hyperdrift/storage/server/models"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
 
-type IConfig interface {
-	GetConfig() *models.Config
-}
-
 type Config struct {
-	config *models.Config
+	AppId          string `json:"app_id" mapstructure:"app_id"`
+	AppName        string `json:"app_name" mapstructure:"app_name"`
+	AppEnvironment string `json:"app_environment" mapstructure:"app_environment"`
+	AppHost        string `json:"app_host" mapstructure:"app_host"`
+	AppPort        string `json:"app_port" mapstructure:"app_port"`
+
+	PostgresUrl string `json:"postgres_url" mapstructure:"postgres_url"`
+
+	S3Endpoint        string `json:"s3_endpoint" mapstructure:"s3_endpoint"`
+	S3AccessKeyId     string `json:"s3_access_key_id" mapstructure:"s3_access_key_id"`
+	S3SecretAccessKey string `json:"s3_secret_access_key" mapstructure:"s3_secret_access_key"`
+	S3Region          string `json:"s3_region" mapstructure:"s3_region"`
+	S3Bucket          string `json:"s3_bucket" mapstructure:"s3_bucket"`
+	S3ForcePathStyle  bool   `json:"s3_force_path_style" mapstructure:"s3_force_path_style"`
+	S3DisableSSL      bool   `json:"s3_disable_ssl" mapstructure:"s3_disable_ssl"`
 }
 
-func NewConfig(viper *viper.Viper, logger *zap.Logger) IConfig {
-	var config models.Config
+func NewConfig(viper *viper.Viper, logger *zap.Logger) *Config {
+	var config Config
 
 	err := viper.ReadInConfig()
 	if err != nil {
@@ -27,20 +36,9 @@ func NewConfig(viper *viper.Viper, logger *zap.Logger) IConfig {
 		logger.Fatal("error unmarshaling config file", zap.Error(err))
 	}
 
-	config.SetDefault()
-
-	err = config.Validate()
 	if err != nil {
 		logger.Fatal("error validating config file", zap.Error(err))
 	}
 
-	var iConfig IConfig = &Config{
-		config: &config,
-	}
-
-	return iConfig
-}
-
-func (c *Config) GetConfig() *models.Config {
-	return c.config
+	return &config
 }
