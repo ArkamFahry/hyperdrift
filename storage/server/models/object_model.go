39eab2f057e8c02c67fed8c87b20b26062540341
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-22 18:40:23 +0530

    chore: added the pre signed object models

--- storage/server/models/object_model.go
+++ storage/server/models/object_model.go
@@ -6,7 +6,7 @@ type Object struct {
 	Id             string     `json:"id"`
 	Name           string     `json:"name"`
 	BucketId       string     `json:"bucket_id"`
-	MimeType       string     `json:"mime_type"`
+	ContentType    string     `json:"content_type"`
 	Size           int64      `json:"size"`
 	Public         bool       `json:"public"`
 	Metadata       []byte     `json:"metadata"`
@@ -14,4 +14,34 @@ type Object struct {
 	LastAccessedAt *time.Time `json:"last_accessed_at"`
 	CreatedAt      time.Time  `json:"created_at"`
 	UpdatedAt      *time.Time `json:"updated_at"`
+}
+
+type PreSignedObject struct {
+	Url       string `json:"url"`
+	Method    string `json:"method"`
+	ExpiresAt int64  `json:"expires_at"`
+}
+
+type CreatePreSignedUploadObject struct {
+	Bucket    string `json:"bucket"`
+	Name      string `json:"name"`
+	ExpiresIn *int64 `json:"expires_in"`
+	MimeType  string `json:"mime_type"`
+	Size      int64  `json:"size"`
+}
+
+type CreatePreSignedDownloadObject struct {
+	Bucket    string `json:"bucket"`
+	Name      string `json:"name"`
+	ExpiresIn *int64 `json:"expires_in"`
+}
+
+type CheckIfObjectExists struct {
+	Bucket string `json:"bucket"`
+	Name   string `json:"name"`
+}
+
+type DeleteObject struct {
+	Bucket string `json:"bucket"`
+	Name   string `json:"name"`
 }
