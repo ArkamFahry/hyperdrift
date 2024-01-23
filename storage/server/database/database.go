5eb0514458092f8a5883e3f93798416b9b33f3bc
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-23 17:23:48 +0530

    chore: added a way to create a database pool

---
+++ storage/server/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"github.com/ArkamFahry/hyperdrift/storage/server/config"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.uber.org/zap"
+	"golang.org/x/net/context"
+)
+
+type Database struct {
+	dbPoll *pgxpool.Pool
+}
+
+func NewDatabasePool(config *config.Config, logger *zap.Logger) *Database {
+	const op = "database.NewDatabasePool"
+
+	db, err := pgxpool.New(context.Background(), config.PostgresUrl)
+	if err != nil {
+		logger.Fatal("failed to connect to create pgx pool database connection",
+			zap.Error(err),
+			zap.String("operation", op),
+		)
+	}
+
+	return &Database{dbPoll: db}
+}
+
+func (d *Database) GetDatabase() (*pgxpool.Conn, error) {
+	return d.dbPoll.Acquire(context.Background())
+}
+func (d *Database) Ping() error {
+	return d.dbPoll.Ping(context.Background())
+}
+
+func (d *Database) Close() {
+	if d.dbPoll != nil {
+		d.dbPoll.Close()
+	}
+}
