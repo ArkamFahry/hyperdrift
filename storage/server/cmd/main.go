bb13d364962cea45672acbc5dba2c8c0e60cc07c
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-23 17:24:08 +0530

    chore: registered migration runner

--- storage/server/cmd/main.go
+++ storage/server/cmd/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"github.com/ArkamFahry/hyperdrift/storage/server/api"
 	"github.com/ArkamFahry/hyperdrift/storage/server/config"
+	"github.com/ArkamFahry/hyperdrift/storage/server/database/migrations"
 	"github.com/ArkamFahry/hyperdrift/storage/server/logger"
 )
 
 func main() {
 	appConfig := config.NewConfig()
+
 	appLogger := logger.NewLogger(appConfig)
+
+	migrations.NewMigrations(appConfig, appLogger)
 
 	api.NewApi(appLogger, appConfig)
 }
