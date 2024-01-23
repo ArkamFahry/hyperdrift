7be63aee77b65caaf8d38810e09c2fab2c2f9b3a
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-23 14:45:58 +0530

    chore: added object creation models

--- storage/server/models/object_model.go
+++ storage/server/models/object_model.go
@@ -16,6 +16,34 @@ type Object struct {
 	UpdatedAt      *time.Time `json:"updated_at"`
 }
 
+type CreateObject struct {
+	Id             string     `json:"id"`
+	Name           string     `json:"name"`
+	BucketId       string     `json:"bucket_id"`
+	ContentType    string     `json:"content_type"`
+	Size           int64      `json:"size"`
+	Public         bool       `json:"public"`
+	Metadata       []byte     `json:"metadata"`
+	UploadStatus   string     `json:"upload_status"`
+	LastAccessedAt *time.Time `json:"last_accessed_at"`
+	CreatedAt      time.Time  `json:"created_at"`
+}
+
+type RenameObject struct {
+	OldName string `json:"old_name"`
+	NewName string `json:"new_name"`
+}
+
+type CopyObject struct {
+	OldPath string `json:"old_path"`
+	NewPath string `json:"new_path"`
+}
+
+type MoveObject struct {
+	OldPath string `json:"old_path"`
+	NewPath string `json:"new_path"`
+}
+
 type PreSignedObject struct {
 	Url       string `json:"url"`
 	Method    string `json:"method"`
@@ -23,11 +51,11 @@ type PreSignedObject struct {
 }
 
 type CreatePreSignedUploadObject struct {
-	Bucket    string `json:"bucket"`
-	Name      string `json:"name"`
-	ExpiresIn *int64 `json:"expires_in"`
-	MimeType  string `json:"mime_type"`
-	Size      int64  `json:"size"`
+	Bucket      string `json:"bucket"`
+	Name        string `json:"name"`
+	ExpiresIn   *int64 `json:"expires_in"`
+	ContentType string `json:"content_type"`
+	Size        int64  `json:"size"`
 }
 
 type CreatePreSignedDownloadObject struct {
