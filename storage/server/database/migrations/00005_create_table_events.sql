e3a54497d3d826d7034ab442e187da4d9ee13d44
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-28 17:46:56 +0530

    refactor: updated event model to be more complete with retry info and version info

--- storage/server/database/migrations/00005_create_table_events.sql
+++ storage/server/database/migrations/00005_create_table_events.sql
@@ -3,15 +3,19 @@
 
 create table if not exists storage.events
 (
-    id text not null check ( storage.text_non_empty_trimmed_text(id) ),
-    name text not null check ( storage.text_non_empty_trimmed_text(name) ),
-    payload jsonb not null,
-    status text        default 'pending'                  not null check (
-        status in ('pending', 'in_progress', 'processing', 'completed', 'failed')
+    id         text                          not null check ( storage.text_non_empty_trimmed_text(id) ),
+    version    int         default 0         not null check ( version >= 0 ),
+    name       text                          not null check ( storage.text_non_empty_trimmed_text(name) ),
+    content    jsonb                         not null,
+    status     text        default 'pending' not null check (
+        status in ('pending', 'processing', 'completed', 'failed')
         ),
-    producer text default 'hyperdrift-storage' not null check ( storage.text_non_empty_trimmed_text(producer) ),
-    timestamp timestamptz default now() not null,
-    constraint events_id_pk primary key (id)
+    retries    int         default 0         not null check ( retries >= 0 ),
+    expires_at timestamptz,
+    created_at timestamptz default now()     not null,
+    constraint events_id_pk primary key (id),
+    constraint events_id_version_uq unique (id, version),
+    constraint events_name_uq unique (name)
 );
 
 -- +goose StatementEnd
