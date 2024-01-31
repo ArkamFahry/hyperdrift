975769bddf6037f9082a0552da29ddc3dfcc9ab5
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-31 10:40:06 +0530

    refactor: minor name change

--- storage/server/bucket/bucket_storage.go
+++ storage/server/bucket/bucket_storage.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/ArkamFahry/hyperdrift/storage/server/bucket/dto"
 	"github.com/ArkamFahry/hyperdrift/storage/server/common/config"
+	"github.com/ArkamFahry/hyperdrift/storage/server/common/zapfield"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"go.uber.org/zap"
@@ -29,20 +30,26 @@ func NewBucketStorage(s3Client *s3.Client, config *config.Config, logger *zap.Lo
 func (s *BucketStorage) EmptyBucket(ctx context.Context, emptyBucket *dto.BucketEmpty) error {
 	const op = "BucketStorage.EmptyBucket"
 
-	key := createS3Key(emptyBucket.Id)
+	key := createS3BucketPathKey(emptyBucket.Id)
 
 	_, err := s.s3Client.ListObjects(ctx, &s3.ListObjectsInput{
 		Bucket: aws.String(s.bucketName),
 		Prefix: aws.String(key),
 	})
 	if err != nil {
-		s.logger.Error("failed to list objects", zap.Error(err), zap.String("bucket", s.bucketName), zap.String("key", key), zap.String("operation", op))
+		s.logger.Error(
+			"failed to list objects",
+			zap.Error(err),
+			zap.String("bucket", s.bucketName),
+			zap.String("key", key),
+			zapfield.Operation(op),
+		)
 		return err
 	}
 
 	return nil
 }
 
-func createS3Key(bucket string) string {
+func createS3BucketPathKey(bucket string) string {
 	return fmt.Sprintf(`%s/`, bucket)
 }
