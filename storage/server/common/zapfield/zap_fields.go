1e4419ac56713f73f8e934b0fc292542df631278
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-01 17:45:53 +0530

    chore: added zap field request id

--- storage/server/common/zapfield/zap_fields.go
+++ storage/server/common/zapfield/zap_fields.go
@@ -5,3 +5,7 @@ import "go.uber.org/zap"
 func Operation(operation string) zap.Field {
 	return zap.String("operation", operation)
 }
+
+func RequestId(requestId string) zap.Field {
+	return zap.String("request_id", requestId)
+}
