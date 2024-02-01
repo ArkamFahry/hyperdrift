ebe5f6dd4d5b9169b3a18088f39d90bb6f6cc577
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-01 11:10:49 +0530

    chore: added dtos to be used in service layer

--- storage/server/bucket/dto/bucket_dto.go
+++ storage/server/bucket/dto/bucket_dto.go
@@ -2,6 +2,27 @@ package dto
 
 import "time"
 
+type Bucket struct {
+	Id                   string     `json:"id"`
+	Version              int32      `json:"version"`
+	Name                 string     `json:"name"`
+	AllowedContentTypes  []string   `json:"allowed_content_types"`
+	MaxAllowedObjectSize *int64     `json:"max_allowed_object_size"`
+	Public               bool       `json:"public"`
+	Disabled             bool       `json:"enabled"`
+	Locked               bool       `json:"locked"`
+	LockReason           *string    `json:"lock_reason"`
+	LockedAt             *time.Time `json:"locked_at"`
+	CreatedAt            time.Time  `json:"created_at"`
+	UpdatedAt            *time.Time `json:"updated_at"`
+}
+
+type BucketSize struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Size int64  `json:"size"`
+}
+
 type BucketCreate struct {
 	Id                   string   `json:"id"`
 	Name                 string   `json:"name"`
@@ -32,16 +53,6 @@ type BucketMakePublic struct {
 }
 
 type BucketMakePrivate struct {
-	Id string `json:"id"`
-}
-
-type BucketLock struct {
-	Id         string     `json:"id"`
-	LockReason string     `json:"lock_reason"`
-	LockedAt   *time.Time `json:"locked_at"`
-}
-
-type BucketUnlock struct {
 	Id string `json:"id"`
 }
 
