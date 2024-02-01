2ac496b9ae4f37b5b589956ab7a6a444cbb4a96b
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-01 11:11:17 +0530

    chore: added entities to be used in service layer

--- storage/server/bucket/entities/bucket_entity.go
+++ storage/server/bucket/entities/bucket_entity.go
@@ -4,6 +4,7 @@ import "time"
 
 type Bucket struct {
 	Id                   string     `json:"id"`
+	Version              int32      `json:"version"`
 	Name                 string     `json:"name"`
 	AllowedContentTypes  []string   `json:"allowed_content_types"`
 	MaxAllowedObjectSize *int64     `json:"max_allowed_object_size"`
@@ -14,4 +15,42 @@ type Bucket struct {
 	LockedAt             *time.Time `json:"locked_at"`
 	CreatedAt            time.Time  `json:"created_at"`
 	UpdatedAt            *time.Time `json:"updated_at"`
+}
+
+type BucketSize struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Size int64  `json:"size"`
+}
+
+type BucketCreate struct {
+	Id                   string   `json:"id"`
+	Name                 string   `json:"name"`
+	AllowedContentTypes  []string `json:"allowed_content_types"`
+	MaxAllowedObjectSize *int64   `json:"max_allowed_object_size"`
+	Public               bool     `json:"public"`
+	Disabled             bool     `json:"enabled"`
+}
+
+type BucketUpdate struct {
+	Id                   string `json:"id"`
+	MaxAllowedObjectSize *int64 `json:"max_allowed_object_size"`
+	Public               *bool  `json:"public"`
+	Version              int32  `json:"version"`
+}
+
+type BucketAllowedContentTypesUpdate struct {
+	Id                  string   `json:"id"`
+	AllowedContentTypes []string `json:"allowed_content_types"`
+	Version             int32    `json:"version"`
+}
+
+type BucketEnable struct {
+	Id      string `json:"id"`
+	Version int32  `json:"version"`
+}
+
+type BucketDisable struct {
+	Id      string `json:"id"`
+	Version int32  `json:"version"`
 }
