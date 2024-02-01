6813f61cc31fe2183f5a0e9d64ae81451fd5263b
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-01 11:14:04 +0530

    chore: wired the BucketService with controller

--- storage/server/bucket/bucket_controller.go
+++ storage/server/bucket/bucket_controller.go
@@ -1,6 +1,7 @@
 package bucket
 
 import (
+	"github.com/ArkamFahry/hyperdrift/storage/server/bucket/dto"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -30,23 +31,71 @@ func (bc *BucketController) RegisterBucketRoutes(app *fiber.App) {
 	routesV1.Delete("/buckets/:id", bc.DeleteBucket)
 	routesV1.Get("/buckets/:id", bc.GetBucket)
 	routesV1.Get("/buckets/:id/size", bc.GetBucketSize)
-	routesV1.Get("/buckets", bc.ListBuckets)
+	routesV1.Get("/all-buckets", bc.ListAllBuckets)
 }
 
 func (bc *BucketController) CreateBucket(ctx *fiber.Ctx) error {
-	return nil
+	var bucketCreate dto.BucketCreate
+
+	err := ctx.BodyParser(&bucketCreate)
+	if err != nil {
+		return err
+	}
+
+	createdBucket, err := bc.bucketService.CreateBucket(ctx.Context(), &bucketCreate)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(createdBucket)
 }
 
 func (bc *BucketController) UpdateBucket(ctx *fiber.Ctx) error {
-	return nil
+	var bucketUpdate dto.BucketUpdate
+
+	err := ctx.BodyParser(&bucketUpdate)
+	if err != nil {
+		return err
+	}
+
+	updatedBucket, err := bc.bucketService.UpdateBucket(ctx.Context(), &bucketUpdate)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(updatedBucket)
 }
 
 func (bc *BucketController) AddAllowedContentTypesToBucket(ctx *fiber.Ctx) error {
-	return nil
+	var bucketAddAllowedContentTypes dto.BucketAddAllowedContentTypes
+
+	err := ctx.BodyParser(&bucketAddAllowedContentTypes)
+	if err != nil {
+		return err
+	}
+
+	contentTypesAddedBucket, err := bc.bucketService.AddAllowedContentTypesToBucket(ctx.Context(), &bucketAddAllowedContentTypes)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(contentTypesAddedBucket)
 }
 
 func (bc *BucketController) RemoveAllowedContentTypesFromBucket(ctx *fiber.Ctx) error {
-	return nil
+	var bucketRemoveAllowedContentTypes dto.BucketRemoveAllowedContentTypes
+
+	err := ctx.BodyParser(&bucketRemoveAllowedContentTypes)
+	if err != nil {
+		return err
+	}
+
+	contentTypesRemovedBucket, err := bc.bucketService.RemoveContentTypesFromBucket(ctx.Context(), &bucketRemoveAllowedContentTypes)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(contentTypesRemovedBucket)
 }
 
 func (bc *BucketController) EmptyBucket(ctx *fiber.Ctx) error {
@@ -58,23 +107,65 @@ func (bc *BucketController) RenameBucket(ctx *fiber.Ctx) error {
 }
 
 func (bc *BucketController) DisableBucket(ctx *fiber.Ctx) error {
-	return nil
+	id := ctx.Params("id")
+
+	disabledBucket, err := bc.bucketService.DisableBucket(ctx.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(disabledBucket)
 }
 
 func (bc *BucketController) EnableBucket(ctx *fiber.Ctx) error {
-	return nil
+	id := ctx.Params("id")
+
+	enabledBucket, err := bc.bucketService.EnableBucket(ctx.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(enabledBucket)
 }
 
 func (bc *BucketController) DeleteBucket(ctx *fiber.Ctx) error {
-	return nil
+	id := ctx.Params("id")
+
+	err := bc.bucketService.DeleteBucket(ctx.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return ctx.SendStatus(fiber.StatusNoContent)
 }
 
 func (bc *BucketController) GetBucket(ctx *fiber.Ctx) error {
-	return nil
+	id := ctx.Params("id")
+
+	bucket, err := bc.bucketService.GetBucket(ctx.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(bucket)
 }
 
-func (bc *BucketController) GetBucketSize(ctx *fiber.Ctx) error { return nil }
+func (bc *BucketController) GetBucketSize(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
 
-func (bc *BucketController) ListBuckets(ctx *fiber.Ctx) error {
-	return nil
+	bucketSize, err := bc.bucketService.GetBucketSize(ctx.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(bucketSize)
+}
+
+func (bc *BucketController) ListAllBuckets(ctx *fiber.Ctx) error {
+	buckets, err := bc.bucketService.ListAllBuckets(ctx.Context())
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(buckets)
 }
