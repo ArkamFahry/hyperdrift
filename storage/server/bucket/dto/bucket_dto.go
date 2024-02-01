959b4e8908c4eea4961e8a9f918ad0dc071b93dc
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-01 20:13:32 +0530

    chore: removed id from update dtos

--- storage/server/bucket/dto/bucket_dto.go
+++ storage/server/bucket/dto/bucket_dto.go
@@ -10,17 +10,14 @@ type BucketCreate struct {
 }
 
 type BucketUpdate struct {
-	Id                   string `json:"id"`
 	MaxAllowedObjectSize *int64 `json:"max_allowed_object_size"`
 	Public               *bool  `json:"public"`
 }
 
 type BucketAddAllowedContentTypes struct {
-	Id                  string   `json:"id"`
-	AllowedContentTypes []string `json:"allowed_content_types"`
+	AddContentTypes []string `json:"add_content_types"`
 }
 
 type BucketRemoveAllowedContentTypes struct {
-	Id                  string   `json:"id"`
-	AllowedContentTypes []string `json:"allowed_content_types"`
+	RemoveContentTypes []string `json:"remove_content_types"`
 }
