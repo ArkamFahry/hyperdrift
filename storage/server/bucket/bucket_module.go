2b4a05a36d2a423f096a06da3ba68427a9e5864a
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-03 16:53:46 +0530

    chore: made the bucket module a independent unit for all bucket related operations

--- storage/server/bucket/bucket_module.go
+++ storage/server/bucket/bucket_module.go
@@ -1,1 +1,21 @@
 package bucket
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/riverqueue/river"
+	"go.uber.org/zap"
+)
+
+type BucketModule struct {
+	app    *fiber.App
+	db     *pgxpool.Pool
+	logger *zap.Logger
+	job    *river.Client[pgx.Tx]
+}
+
+func NewBucketModule(app *fiber.App, db *pgxpool.Pool, logger *zap.Logger, job *river.Client[pgx.Tx]) {
+	bucketService := NewBucketService(db, logger, job)
+	NewBucketController(bucketService).RegisterBucketRoutes(app)
+}
