a92ad88a1856ac1102ecdce69157d159e353c1c2
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-03 16:54:15 +0530

    refactor: moved all the startup to the app module

--- storage/server/app_module.go
+++ storage/server/app_module.go
@@ -1,28 +1,66 @@
 package main
 
 import (
+	"context"
+	"github.com/ArkamFahry/hyperdrift/storage/server/bucket"
 	"github.com/ArkamFahry/hyperdrift/storage/server/common/config"
+	"github.com/ArkamFahry/hyperdrift/storage/server/common/database/migrations"
+	"github.com/ArkamFahry/hyperdrift/storage/server/common/logger"
+	"github.com/ArkamFahry/hyperdrift/storage/server/common/zapfield"
 	"github.com/gofiber/contrib/fiberzap/v2"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/riverqueue/river"
+	"github.com/riverqueue/river/riverdriver/riverpgxv5"
 	"go.uber.org/zap"
 )
 
-func NewAppModule(logger *zap.Logger, config *config.Config) {
-	app := fiber.New(fiber.Config{
+func NewAppModule() {
+	const op = "AppModule.NewAppModule"
+
+	appConfig := config.NewConfig()
+
+	appLogger := logger.NewLogger(appConfig)
+
+	migrations.NewMigrations(appConfig, appLogger)
+
+	appServer := fiber.New(fiber.Config{
 		Immutable: true,
 	})
 
-	app.Use(fiberzap.New(fiberzap.Config{
-		Logger: logger,
+	appServer.Use(fiberzap.New(fiberzap.Config{
+		Logger: appLogger,
 	}))
 
-	port := config.ServerPort
+	port := appConfig.ServerPort
 
-	err := app.Listen(":" + port)
+	pgxPool, err := pgxpool.New(context.Background(), appConfig.PostgresUrl)
 	if err != nil {
-		logger.Fatal("error starting fiber server",
+		appLogger.Fatal("error connecting to postgres",
+			zap.Error(err),
+			zapfield.Operation(op),
+		)
+	}
+
+	riverPgx := riverpgxv5.New(pgxPool)
+
+	riverClient, err := river.NewClient[pgx.Tx](riverPgx, nil)
+	if err != nil {
+		appLogger.Fatal("error creating river client",
+			zap.Error(err),
+			zapfield.Operation(op),
+		)
+	}
+
+	bucket.NewBucketModule(appServer, pgxPool, appLogger, riverClient)
+
+	err = appServer.Listen(":" + port)
+	if err != nil {
+		appLogger.Fatal("error starting fiber server",
 			zap.Error(err),
 			zap.String("port", port),
+			zapfield.Operation(op),
 		)
 	}
 }
