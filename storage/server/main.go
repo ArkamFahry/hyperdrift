df9da8c8d6e368c401d979c5e2446d597317a1f8
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-03 16:54:28 +0530

    chore: cleaned up main

--- storage/server/main.go
+++ storage/server/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"github.com/ArkamFahry/hyperdrift/storage/server/common/config"
-	"github.com/ArkamFahry/hyperdrift/storage/server/common/database/migrations"
-	"github.com/ArkamFahry/hyperdrift/storage/server/common/logger"
-)
-
 func main() {
-	appConfig := config.NewConfig()
-
-	appLogger := logger.NewLogger(appConfig)
-
-	migrations.NewMigrations(appConfig, appLogger)
-
-	NewAppModule(appLogger, appConfig)
+	NewAppModule()
 }
