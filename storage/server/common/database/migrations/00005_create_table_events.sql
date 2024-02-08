a0f79a90cc11046fa098bb90efc5741955279bb8
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-08 21:55:54 +0530

    chore: removed setting defaults with functions

--- storage/server/common/database/migrations/00005_create_table_events.sql
+++ storage/server/common/database/migrations/00005_create_table_events.sql
@@ -3,25 +3,13 @@
 
 create table if not exists storage.events
 (
-    id             text  not null check ( storage.text_non_empty_trimmed_text(id) ),
+    id             text default 'event_' || gen_random_uuid() not null check ( storage.text_non_empty_trimmed_text(id) ),
     aggregate_type text  not null check ( storage.text_non_empty_trimmed_text(aggregate_type) ),
     aggregate_id   text  not null check ( storage.text_non_empty_trimmed_text(aggregate_id) ),
     type           text  not null check ( storage.text_non_empty_trimmed_text(type) ),
     payload        jsonb null,
     constraint events_id_pk primary key (id)
 );
-
-create or replace trigger events_set_updated_at
-    before update
-    on storage.events
-    for each row
-execute function storage.set_updated_at();
-
-create or replace trigger events_increment_version
-    before update
-    on storage.events
-    for each row
-execute function storage.increment_version();
 
 -- +goose StatementEnd
 
