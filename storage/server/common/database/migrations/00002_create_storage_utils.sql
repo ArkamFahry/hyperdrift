3934c90019427d9f799a5a10bcc43339a2cef5f7
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-09 08:04:02 +0530

    refactor: swapped out the uuid's for ulid's

--- storage/server/common/database/migrations/00002_create_storage_utils.sql
+++ storage/server/common/database/migrations/00002_create_storage_utils.sql
@@ -1,6 +1,67 @@
 -- +goose Up
 -- +goose StatementBegin
 
+create extension if not exists pgcrypto;
+
+create or replace function storage.gen_random_ulid()
+    returns text
+as
+$$
+declare
+    -- Crockford's Base32
+    encoding  bytea = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
+    timestamp bytea = E'\\000\\000\\000\\000\\000\\000';
+    output    text  = '';
+    unix_time bigint;
+    ulid      bytea;
+BEGIN
+    unix_time = (extract(epoch from clock_timestamp()) * 1000)::bigint;
+    timestamp = set_byte(timestamp, 0, (unix_time >> 40)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 1, (unix_time >> 32)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 2, (unix_time >> 24)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 3, (unix_time >> 16)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 4, (unix_time >> 8)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 5, unix_time::bit(8)::integer);
+
+    -- 10 entropy bytes
+    ulid = timestamp || gen_random_bytes(10);
+
+    -- Encode the timestamp
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 0) & 224) >> 5));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 0) & 31)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 1) & 248) >> 3));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 1) & 7) << 2) | ((get_byte(ulid, 2) & 192) >> 6)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 2) & 62) >> 1));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 2) & 1) << 4) | ((get_byte(ulid, 3) & 240) >> 4)));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 3) & 15) << 1) | ((get_byte(ulid, 4) & 128) >> 7)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 4) & 124) >> 2));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 4) & 3) << 3) | ((get_byte(ulid, 5) & 224) >> 5)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 5) & 31)));
+
+    -- Encode the entropy
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 6) & 248) >> 3));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 6) & 7) << 2) | ((get_byte(ulid, 7) & 192) >> 6)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 7) & 62) >> 1));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 7) & 1) << 4) | ((get_byte(ulid, 8) & 240) >> 4)));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 8) & 15) << 1) | ((get_byte(ulid, 9) & 128) >> 7)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 9) & 124) >> 2));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 9) & 3) << 3) | ((get_byte(ulid, 10) & 224) >> 5)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 10) & 31)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 11) & 248) >> 3));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 11) & 7) << 2) | ((get_byte(ulid, 12) & 192) >> 6)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 12) & 62) >> 1));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 12) & 1) << 4) | ((get_byte(ulid, 13) & 240) >> 4)));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 13) & 15) << 1) | ((get_byte(ulid, 14) & 128) >> 7)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 14) & 124) >> 2));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 14) & 3) << 3) | ((get_byte(ulid, 15) & 224) >> 5)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 15) & 31)));
+
+    RETURN output;
+END
+$$
+    LANGUAGE plpgsql
+    VOLATILE;
+
 create or replace function storage.text_non_empty_trimmed_text(val text) returns boolean as
 $$
 begin
@@ -69,7 +130,7 @@ create or replace function storage.on_create()
     returns trigger as
 $$
 begin
-    new.id = tg_table_name || '_' || gen_random_uuid();
+    new.id = tg_table_name || '_' || storage.gen_random_ulid();
     new.version = 0;
     new.created_at = now();
 
@@ -77,7 +138,7 @@ begin
 end;
 $$ language plpgsql;
 
-create or replace function util.on_update()
+create or replace function storage.on_update()
     returns trigger as
 $$
 begin
@@ -93,9 +154,9 @@ $$ language plpgsql;
 -- +goose Down
 -- +goose StatementBegin
 
-drop function if exists storage.increment_version();
+drop function if exists storage.on_update();
 
-drop function if exists storage.set_updated_at();
+drop function if exists storage.on_create();
 
 drop function if exists storage.bigint_null_or_non_zero_bigint();
 
@@ -106,5 +167,7 @@ drop function if exists storage.array_null_or_contains_empty_trimmed_text();
 drop function if exists storage.text_null_or_non_empty_trimmed_text();
 
 drop function if exists storage.text_non_empty_trimmed_text();
+
+drop function if exists storage.gen_random_ulid();
 
 -- +goose StatementEnd
