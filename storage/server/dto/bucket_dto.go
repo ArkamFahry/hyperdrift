d3e7e0b64ac408ff350d23d753abec9f77561919
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-10 22:10:23 +0530

    refactor: removed Id from BucketCreate dto

--- storage/server/dto/bucket_dto.go
+++ storage/server/dto/bucket_dto.go
@@ -1,7 +1,6 @@
 package dto
 
 type BucketCreate struct {
-	Id                   string   `json:"id"`
 	Name                 string   `json:"name"`
 	AllowedContentTypes  []string `json:"allowed_content_types"`
 	MaxAllowedObjectSize *int64   `json:"max_allowed_object_size"`
