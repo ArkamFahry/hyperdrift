3b520ae2ea9b71d5efa1f3b99537f5f5959a0005
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-11 10:40:31 +0530

    chore: added more context to request logger middleware

--- storage/server/middleware/logger_middleware.go
+++ storage/server/middleware/logger_middleware.go
@@ -10,6 +10,17 @@ func Logger(logger *zap.Logger) fiber.Handler {
 	return fiberzap.New(
 		fiberzap.Config{
 			Logger: logger,
+			FieldsFunc: func(c *fiber.Ctx) []zap.Field {
+				return []zap.Field{
+					zap.Int("status", c.Response().StatusCode()),
+					zap.String("method", c.Method()),
+					zap.String("route", c.Route().Path),
+					zap.String("path", c.Path()),
+					zap.String("ip", c.IP()),
+					zap.String("user-agent", c.Get("User-Agent")),
+					zap.String("request-id", c.Context().Value("request_id").(string)),
+				}
+			},
 		},
 	)
 }
