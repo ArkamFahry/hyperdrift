d963a37e93552cdc35cae83d15e3e15d38f73ffd
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-13 19:05:18 +0530

    chore: added openapi annotation comments to generate openapi docs

--- storage/server/controllers/object_controller.go
+++ storage/server/controllers/object_controller.go
@@ -25,10 +25,22 @@ func (oc *ObjectController) RegisterObjectRoutes(app *fiber.App) {
 	routesV1.Post("/objects/:bucket_name/pre-signed/upload/:object_id/complete", oc.CompletePreSignedObjectUpload)
 	routesV1.Get("/objects/:bucket_name/pre-signed/download/:object_id", oc.CreatePreSignedDownloadObject)
 	routesV1.Delete("/objects/:bucket_name/:object_id", oc.DeleteObject)
-	routesV1.Get("/objects/:bucket_name/:object_id", oc.GetObjectById)
-	routesV1.Get("/objects/:bucket_name/:object_path", oc.SearchObjectsByBucketNameAndObjectPath)
+	routesV1.Get("/objects/:bucket_name/:object_id", oc.GetObject)
+	routesV1.Get("/objects/:bucket_name/:object_path", oc.SearchObjects)
 }
 
+// CreatePreSignedUploadObject is used to create a pre signed upload object
+// @Summary Create a pre signed upload object
+// @Description Create a pre signed upload object
+// @Tags objects
+// @Accept json
+// @Produce json
+// @Param bucket_name path string true "Bucket Name"
+// @Param bucket body models.PreSignedUploadObjectCreate true "Pre Signed Upload Object Create"
+// @Success 201 {object} models.PreSignedUploadObject
+// @Failure 400 {object} middleware.HttpError
+// @Failure 500 {object} middleware.HttpError
+// @Router /api/v1/objects/{bucket_name}/pre-signed/upload [post]
 func (oc *ObjectController) CreatePreSignedUploadObject(ctx *fiber.Ctx) error {
 	var preSignedUploadObjectCreate models.PreSignedUploadObjectCreate
 
@@ -47,6 +59,18 @@ func (oc *ObjectController) CreatePreSignedUploadObject(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusCreated).JSON(preSignedUploadObject)
 }
 
+// CompletePreSignedObjectUpload is used to complete a pre signed upload session
+// @Summary Complete a pre signed upload session
+// @Description Complete a pre signed upload session
+// @Tags objects
+// @Accept json
+// @Produce json
+// @Param bucket_name path string true "Bucket Name"
+// @Param object_id path string true "Object ID"
+// @Success 202
+// @Failure 400 {object} middleware.HttpError
+// @Failure 500 {object} middleware.HttpError
+// @Router /api/v1//objects/{bucket_name}/pre-signed/upload/{object_id}/complete [post]
 func (oc *ObjectController) CompletePreSignedObjectUpload(ctx *fiber.Ctx) error {
 	bucketName := ctx.Params("bucket_name")
 	objectId := ctx.Params("object_id")
@@ -59,6 +83,19 @@ func (oc *ObjectController) CompletePreSignedObjectUpload(ctx *fiber.Ctx) error 
 	return ctx.SendStatus(fiber.StatusAccepted)
 }
 
+// CreatePreSignedDownloadObject is used to create a pre signed download object
+// @Summary Create a pre signed download object
+// @Description Create a pre signed download object
+// @Tags objects
+// @Accept json
+// @Produce json
+// @Param bucket_name path string true "Bucket Name"
+// @Param object_id path string true "Object ID"
+// @Param expires_in query int true "Expires In"
+// @Success 201 {object} models.PreSignedDownloadObject
+// @Failure 400 {object} middleware.HttpError
+// @Failure 500 {object} middleware.HttpError
+// @Router /api/v1/objects/{bucket_name}/pre-signed/download/{object_id} [post]
 func (oc *ObjectController) CreatePreSignedDownloadObject(ctx *fiber.Ctx) error {
 	bucketName := ctx.Params("bucket_name")
 	objectId := ctx.Params("object_id")
@@ -73,6 +110,18 @@ func (oc *ObjectController) CreatePreSignedDownloadObject(ctx *fiber.Ctx) error 
 	return ctx.Status(fiber.StatusOK).JSON(preSignedDownloadObject)
 }
 
+// DeleteObject is used to delete an object
+// @Summary Delete an object
+// @Description Delete an object
+// @Tags objects
+// @Accept json
+// @Produce json
+// @Param bucket_name path string true "Bucket Name"
+// @Param object_id path string true "Object ID"
+// @Success 204
+// @Failure 400 {object} middleware.HttpError
+// @Failure 500 {object} middleware.HttpError
+// @Router /api/v1/objects/{bucket_name}/{object_id} [delete]
 func (oc *ObjectController) DeleteObject(ctx *fiber.Ctx) error {
 	bucketName := ctx.Params("bucket_name")
 	objectId := ctx.Params("object_id")
@@ -85,11 +134,23 @@ func (oc *ObjectController) DeleteObject(ctx *fiber.Ctx) error {
 	return ctx.SendStatus(fiber.StatusNoContent)
 }
 
-func (oc *ObjectController) GetObjectById(ctx *fiber.Ctx) error {
+// GetObject is used to get an object
+// @Summary Get an object
+// @Description Get an object
+// @Tags objects
+// @Accept json
+// @Produce json
+// @Param bucket_name path string true "Bucket Name"
+// @Param object_id path string true "Object ID"
+// @Success 200 {object} models.Object
+// @Failure 400 {object} middleware.HttpError
+// @Failure 500 {object} middleware.HttpError
+// @Router /api/v1/objects/{bucket_name}/{object_id} [get]
+func (oc *ObjectController) GetObject(ctx *fiber.Ctx) error {
 	bucketName := ctx.Params("bucket_name")
 	objectId := ctx.Params("object_id")
 
-	object, err := oc.objectService.GetObjectById(ctx.Context(), bucketName, objectId)
+	object, err := oc.objectService.GetObject(ctx.Context(), bucketName, objectId)
 	if err != nil {
 		return err
 	}
@@ -97,7 +158,22 @@ func (oc *ObjectController) GetObjectById(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).JSON(object)
 }
 
-func (oc *ObjectController) SearchObjectsByBucketNameAndObjectPath(ctx *fiber.Ctx) error {
+// SearchObjects is used to search objects
+// @Summary Search objects by path
+// @Description Search objects by path
+// @Tags objects
+// @Accept json
+// @Produce json
+// @Param bucket_name path string true "Bucket Name"
+// @Param object_path path string true "Object Path"
+// @Param levels query int true "Levels"
+// @Param limit query int true "Limit"
+// @Param offset query int true "Offset"
+// @Success 200 {array} models.Object
+// @Failure 400 {object} middleware.HttpError
+// @Failure 500 {object} middleware.HttpError
+// @Router /api/v1/objects/{bucket_name}/{object_path} [get]
+func (oc *ObjectController) SearchObjects(ctx *fiber.Ctx) error {
 	bucketName := ctx.Params("bucket_name")
 	objectPath := ctx.Params("object_path")
 
@@ -105,7 +181,7 @@ func (oc *ObjectController) SearchObjectsByBucketNameAndObjectPath(ctx *fiber.Ct
 	limit := ctx.QueryInt("limit")
 	offset := ctx.QueryInt("offset")
 
-	objects, err := oc.objectService.SearchObjectsByBucketNameAndObjectPath(ctx.Context(), bucketName, objectPath, int32(levels), int32(limit), int32(offset))
+	objects, err := oc.objectService.SearchObjects(ctx.Context(), bucketName, objectPath, int32(levels), int32(limit), int32(offset))
 	if err != nil {
 		return err
 	}
