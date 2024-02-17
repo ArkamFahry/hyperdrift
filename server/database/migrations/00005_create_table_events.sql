017448bc4784ddff95d584d0e5f3b111ec7c9d92
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-17 14:25:10 +0530

    refactor: renamed type to event_type

--- server/database/migrations/00005_create_table_events.sql
+++ server/database/migrations/00005_create_table_events.sql
@@ -7,7 +7,7 @@ create table if not exists storage.events
     version        int default 0 not null check ( version >= 0 ),
     aggregate_type text          not null check ( storage.text_non_empty_trimmed_text(aggregate_type) ),
     aggregate_id   text          not null check ( storage.text_non_empty_trimmed_text(aggregate_id) ),
-    type           text          not null check ( storage.text_non_empty_trimmed_text(type) ),
+    event_type     text          not null check ( storage.text_non_empty_trimmed_text(event_type) ),
     payload        jsonb         null,
     created_at     timestamptz   not null,
     constraint events_id_primary_key primary key (id)
@@ -17,7 +17,7 @@ create or replace trigger events_on_create
     before insert
     on storage.events
     for each row
-execute function storage.on_create();
+execute function storage.on_create('event');
 
 -- +goose StatementEnd
 
