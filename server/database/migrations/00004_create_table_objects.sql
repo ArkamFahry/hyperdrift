52cc3d117a87985b51d0a3ac69ba516fe7492cdd
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-17 14:22:58 +0530

    chore: removed unused generated path_token field

--- server/database/migrations/00004_create_table_objects.sql
+++ server/database/migrations/00004_create_table_objects.sql
@@ -7,7 +7,6 @@ create table if not exists storage.objects
     version          int         default 0                          not null check ( version >= 0 ),
     bucket_id        text                                           not null check ( storage.text_non_empty_trimmed_text(bucket_id) ),
     name             text                                           not null check ( storage.text_non_empty_trimmed_text(name) ),
-    path_tokens      text[]                                         not null generated always as (string_to_array(name, '/')) stored,
     mime_type        text        default 'application/octet-stream' not null check ( storage.text_non_empty_trimmed_text(mime_type) ),
     size             bigint      default 0                          not null check ( size >= 0 ),
     metadata         jsonb                                          null,
