4f77d356095bd178b0d2b23377762d4a658a77ea
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-02-20 11:11:54 +0530

    chore: removed aggregate_id from insert

--- server/database/queries/event_query.sql
+++ server/database/queries/event_query.sql
@@ -1,7 +1,6 @@
 -- name: EventCreate :one
 insert into storage.events
-    (aggregate_type, aggregate_id, event_type, payload)
+    (aggregate_type, event_type, payload)
 values (sqlc.arg('aggregate_type'),
-        sqlc.arg('aggregate_id'),
         sqlc.arg('event_type'),
         sqlc.narg('payload')) returning id;
